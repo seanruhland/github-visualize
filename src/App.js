f3719119f38ced7c5a8ebc065b17ab9362965331
Sean Ruhland <sean.ruhland@gmail.com>
2020-08-09 18:43:48 -0700

    form made

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
+import Form from './Components/Form'
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className='pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900'>
-        Search a GitHub User
-      </h1>
-    </div>
-  );
+    return (
+      <div>
+        <h1 className='pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900'>
+          Search a GitHub User
+        </h1>
+        <Form />
+      </div>
+    );
 }
 
 export default App;
